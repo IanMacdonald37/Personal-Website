150fc3bf0dbfd88e3d0aa8f49cb27896574c6e5a
IanMacdonald37 <imacdonald37@my.bcit.ca>
2024-01-06 21:28:44 -0800

    basic routing established for projects and resume pages

--- app.py
+++ app.py
@@ -4,3 +4,20 @@ from pathlib import Path
 
 app = Flask(__name__)
 app.instance_path = Path(".").resolve()
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/resume")
+def resume():
+    return render_template("resume.html")
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
